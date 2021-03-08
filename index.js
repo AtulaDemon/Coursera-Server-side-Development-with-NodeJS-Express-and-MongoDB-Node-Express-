c7bbf2ebcbf1842c0d966c429239853b2d058009
Atula Demon <caikinhdenlc@gmail.com>
2021-03-09 02:48:34 +0700

    Course 3 - Week 1: Assignment 1

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http = require('http');
 const morgan = require('morgan');
 
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -12,24 +14,8 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 app.use('/dishes', dishRouter);
-
-app.get('/dishes/:dishId', (req,res,next) => {
-    res.end('Will send detail of the dish: ' + req.params.dishId + ' to you!');
-});
-
-app.post('/dishes/:dishId', (req,res,next) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /dishes/' + req.params.dishId);
-});
-
-app.put('/dishes/:dishId', (req,res,next) => {
-    res.write('Updating the dish: ' + req.params.dishId + '\n')
-    res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
-});
- 
-app.delete('/dishes/:dishId', (req, res, next) => {
-    res.end('Deleting the dish: ' + req.params.dishId);
-});
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 app.use(express.static(__dirname + '/public'));
 
